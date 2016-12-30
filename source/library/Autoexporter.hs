148fa46c815f3fbc247781034ae9c450a788c120
Taylor Fausak <taylor@fausak.me>
2016-12-30 09:48:26 -0600

    Refactor Autoexporter

--- source/library/Autoexporter.hs
+++ source/library/Autoexporter.hs
@@ -1,6 +1,4 @@
-module Autoexporter
-  ( main
-  ) where
+module Autoexporter where
 
 import qualified Data.List as List
 import qualified Data.Maybe as Maybe
@@ -13,40 +11,62 @@ import qualified System.FilePath as FilePath
 main :: IO ()
 main = do
   args <- Environment.getArgs
+  mainWithArgs args
+
+mainWithArgs :: [String] -> IO ()
+mainWithArgs args =
   case args of
-    [name, inputFile, outputFile] -> do
-      let moduleName = makeModuleName name
-      let directory = FilePath.dropExtension inputFile
-      files <- Directory.getDirectoryContents directory
-      let haskellFiles =
-            filter
-              (\f -> List.isSuffixOf ".hs" f || List.isSuffixOf ".lhs" f)
-              files
-      let paths = map (directory FilePath.</>) haskellFiles
-      let modules = List.sort (map makeModuleName paths)
-      let exports =
-            init (unlines (map (\x -> "    module " ++ x ++ ",") modules))
-      let imports = init (unlines (map ("import " ++) modules))
-      let output =
-            unlines
-              [ unwords ["module", moduleName, "("]
-              , exports
-              , ") where"
-              , ""
-              , imports
-              ]
-      writeFile outputFile output
-    _ -> error ("unexpected arguments: " ++ show args)
+    [name, inputFile, outputFile] -> autoexport name inputFile outputFile
+    _ -> fail ("unexpected arguments: " ++ show args)
+
+autoexport :: String -> FilePath -> FilePath -> IO ()
+autoexport name inputFile outputFile = do
+  let moduleName = makeModuleName name
+  let directory = FilePath.dropExtension inputFile
+  files <- Directory.getDirectoryContents directory
+  let output = makeOutput moduleName directory files
+  writeFile outputFile output
 
 makeModuleName :: FilePath -> String
 makeModuleName name =
   let path = FilePath.dropExtension name
       parts = FilePath.splitDirectories path
-      rest =
-        reverse
-          (takeWhile
-             (\x ->
-                Maybe.isJust
-                  (Text.simpleParse x :: Maybe ModuleName.ModuleName))
-             (reverse parts))
+      rest = takeWhileEnd isModuleName parts
   in List.intercalate "." rest
+
+takeWhileEnd :: (a -> Bool) -> [a] -> [a]
+takeWhileEnd p xs = reverse (takeWhile p (reverse xs))
+
+isModuleName :: String -> Bool
+isModuleName x = Maybe.isJust (parseModuleName x)
+
+parseModuleName :: String -> Maybe ModuleName.ModuleName
+parseModuleName = Text.simpleParse
+
+makeOutput :: String -> FilePath -> [FilePath] -> String
+makeOutput moduleName directory files =
+  let haskellFiles = filter isHaskellFile files
+      paths = map (directory FilePath.</>) haskellFiles
+      modules = List.sort (map makeModuleName paths)
+  in renderModule moduleName modules
+
+isHaskellFile :: FilePath -> Bool
+isHaskellFile x = List.isSuffixOf ".hs" x || List.isSuffixOf ".lhs" x
+
+renderModule :: String -> [String] -> String
+renderModule name modules =
+  unlines'
+    [ unwords ["module", name, "("]
+    , unlines' (map renderExport modules)
+    , ") where"
+    , unlines' (map renderImport modules)
+    ]
+
+unlines' :: [String] -> String
+unlines' = List.intercalate "\n"
+
+renderExport :: String -> String
+renderExport x = "  module " ++ x ++ ","
+
+renderImport :: String -> String
+renderImport x = "import " ++ x
